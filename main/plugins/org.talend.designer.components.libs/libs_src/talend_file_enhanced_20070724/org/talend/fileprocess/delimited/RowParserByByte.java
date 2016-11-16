014da4919d5ac32be6f10c2d7dfcc34131e05922
pyzhou-talend <pyzhou@talend.com>
2016-11-16 17:24:39 +0800

    TDI-37712 : correct format issue.

--- main/plugins/org.talend.designer.components.libs/libs_src/talend_file_enhanced_20070724/org/talend/fileprocess/delimited/RowParserByByte.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend_file_enhanced_20070724/org/talend/fileprocess/delimited/RowParserByByte.java
@@ -15,17 +15,15 @@ public class RowParserByByte {
 
 	private BufferedInputStream in = null;
 
-
 	private boolean safetySwitch = false;
 
-	public  final int MAX_SIZE_FOR_SAFTY = 100000;
+	public final int MAX_SIZE_FOR_SAFTY = 100000;
 
 	private boolean closed = false;
 
 	private long currentRecord = 0;
-	
+
 	private int header = 0;
-	
 
 	public RowParserByByte(String filename, int row_length) throws FileNotFoundException {
 		this.row_length = row_length;
@@ -37,7 +35,7 @@ public class RowParserByByte {
 
 		in = new BufferedInputStream(new FileInputStream(f));
 	}
-	
+
 	public RowParserByByte(ZipInputStream zis, int row_length) throws FileNotFoundException {
 		this.row_length = row_length;
 		in = new BufferedInputStream(zis);
@@ -48,26 +46,27 @@ public class RowParserByByte {
 		return toByteArray();
 
 	}
-	
-    /**
-     * DOC ke Comment method "isSafetySwitch".
-     * 
-     * @return the state of the safetySwitch
-     */
-    public boolean isSafetySwitch() {
-        return safetySwitch;
-    }
-    
-    /**
-     * DOC ke Comment method "setSafetySwitch". When reading a delmited text, if safetySwitch is true, and if a column's
-     * content length is larger than 100,000 or a row's column count is larger than 100,000, an Exception will be
-     * thrown, and the process of reading delimited data will be terminated.
-     * 
-     * @param safetySwitch
-     */
-    public void setSafetySwitch(boolean safetySwitch) {
-        this.safetySwitch = safetySwitch;
-    }
+
+	/**
+	 * DOC ke Comment method "isSafetySwitch".
+	 * 
+	 * @return the state of the safetySwitch
+	 */
+	public boolean isSafetySwitch() {
+		return safetySwitch;
+	}
+
+	/**
+	 * DOC ke Comment method "setSafetySwitch". When reading a delmited text, if
+	 * safetySwitch is true, and if a column's content length is larger than
+	 * 100,000 or a row's column count is larger than 100,000, an Exception will
+	 * be thrown, and the process of reading delimited data will be terminated.
+	 * 
+	 * @param safetySwitch
+	 */
+	public void setSafetySwitch(boolean safetySwitch) {
+		this.safetySwitch = safetySwitch;
+	}
 
 	/**
 	 * DOC ke Comment method "checkClosed". This will be call before read new
@@ -95,7 +94,7 @@ public class RowParserByByte {
 		if (header <= 0) {
 			return;
 		}
-		this.header=header;
+		this.header = header;
 		for (int i = 0; i < header; i++) {
 			readRecord();
 		}
@@ -106,27 +105,26 @@ public class RowParserByByte {
 	public byte[] getBuffer() {
 		return buffer;
 	}
-	
-	public String getRowRecord(){
+
+	public String getRowRecord() {
 		return new String(buffer);
 	}
 
 	public boolean toByteArray() throws IOException {
 
 		this.buffer = new byte[row_length];
-		if (-1 !=  in.read(buffer, 0, row_length)) {
-			
-			if (safetySwitch && currentRecord - header > this.MAX_SIZE_FOR_SAFTY) {
-                close();
+		if (-1 != in.read(buffer, 0, row_length)) {
 
-                throw new IOException(
-                        "Maximum row record length of 100,000 exceeded in row in record "
-                                + NumberFormat.getIntegerInstance().format(currentRecord)
-                                + ". Set the safetySwitch property to false"
-                                + " if you're expecting row record lengths greater than 100,000 characters to"
-                                + " avoid this error.");
-            }
-			
+			if (safetySwitch && currentRecord - header > this.MAX_SIZE_FOR_SAFTY) {
+				close();
+
+				throw new IOException("Maximum row record length of 100,000 exceeded in row in record "
+						+ NumberFormat.getIntegerInstance().format(currentRecord)
+						+ ". Set the safetySwitch property to false"
+						+ " if you're expecting row record lengths greater than 100,000 characters to"
+						+ " avoid this error.");
+			}
+
 			currentRecord++;
 			return true;
 		} else {
